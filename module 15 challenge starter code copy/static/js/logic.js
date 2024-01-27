cd29a57341c7ff456051662f5299dc3af8d6f9fc
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2024-01-27 16:09:09 -0500

    update

--- module 15 challenge starter code copy/static/js/logic.js
+++ module 15 challenge starter code copy/static/js/logic.js
@@ -1,9 +1,23 @@
 function init(){
-const bbData = 
+const eqData = 
 "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
     // Fetch the JSON data and console log it
     d3.json(eqData).then(function(data) {
     //lets show some stuff
-    console.log(data);
+    console.log(data);//works!
+    //okay so what info do we need..
+    //cooridates and magnitide, "features" has it..
+    const eqFeatures = data.features;
+    console.log(eqFeatures);//works!
+    //lets find coordinates
+    //const eqCoordinates = eqFeatures.geometry;
+    //^^why no show geometry..because of the objects!^^
+    //so we need to get each feature first...
+    eqFeatures.forEach(feature => {
+        const eqGeometry = feature.geometry;
+
+
+    });//for eqFeatures.forEach
     });
 };
+init();
