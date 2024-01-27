7af276380f4a0ab74520c55bbef80c650cdaec82
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2024-01-27 15:28:21 -0500

    update

--- module 15 challenge starter code copy/static/js/logic.js
+++ module 15 challenge starter code copy/static/js/logic.js
@@ -1,9 +1,9 @@
+function init(){
 const bbData = 
 "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-// Fetch the JSON data and console log it
-d3.json(eqData).then(function(data) {
-  //lets show some stuff
-  const bbParticipants = data.metadata;
-  const bbParticipantSamples = data.samples;
-  //get through each name
-  bbParticipants.forEach(bbParticipantDemos =>{
+    // Fetch the JSON data and console log it
+    d3.json(eqData).then(function(data) {
+    //lets show some stuff
+    console.log(data);
+    });
+};
